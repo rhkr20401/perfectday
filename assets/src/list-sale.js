d1429bee8b2b9b0d28ab83619ab9399f50b33435
rhkdlf92 <rhkdlf92@gmail.com>
2025-11-28 15:37:12 +0900

    init+

--- assets/src/list-sale.js
+++ assets/src/list-sale.js
@@ -1,41 +1,46 @@
-$(function(){
+export default{
+  init(root){
+    $(function(){
   
-  //chips
-  $('.category-chips li').on('click',function(){
-    const category = $(this).data('category');
-    $(this).addClass('active').siblings().removeClass('active');
+      //chips
+      $('.category-chips li').on('click',function(){
+        const category = $(this).data('category');
+        $(this).addClass('active').siblings().removeClass('active');
 
-    if(category==='all'){
-      $('.category-list li').show();
-    }else{
-      $('.category-list li').hide().filter(`[data-category="${category}"]`).show();
-    }
-  });
+        if(category==='all'){
+          $('.category-list li').show();
+        }else{
+          $('.category-list li').hide().filter(`[data-category="${category}"]`).show();
+        }
+      });
 
-  //chips
-  $('.cost-chips li').on('click',function(){
-    const cost = $(this).data('cost');
-    $(this).addClass('active').siblings().removeClass('active');
+      //chips
+      $('.cost-chips li').on('click',function(){
+        const cost = $(this).data('cost');
+        $(this).addClass('active').siblings().removeClass('active');
 
-    if(cost==='all'){
+        if(cost==='all'){
 
-      $('.cost-list li').show();
-    }else{
-      $('.cost-list li').hide().filter(`[data-cost="${cost}"]`).show();
-    }
-  });
+          $('.cost-list li').show();
+        }else{
+          $('.cost-list li').hide().filter(`[data-cost="${cost}"]`).show();
+        }
+      });
 
-  //icon-heart-wish
-  $('.icon-heart-wish').on('click',function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).toggleClass('active');
-  });
+      //icon-heart-wish
+      $('.icon-heart-wish').on('click',function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).toggleClass('active');
+      });
 
-  //footer company
-  $('footer .company').on('click',function(){
-    $(this).toggleClass('active');
-    $(this).find('.company-info').stop().slideToggle(300);
-  });
-});
+      //footer company
+      $('footer .company').on('click',function(){
+        $(this).toggleClass('active');
+        $(this).find('.company-info').stop().slideToggle(300);
+      });
+    });
+  }
+}
+
 
